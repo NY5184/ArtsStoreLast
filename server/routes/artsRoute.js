47593e435287ccdb1421c466e98a0b40884514f0
NY5184 <yafen2005@gmail.com>
2025-03-12 19:33:11 +0200

    installed hook

--- server/routes/artsRoute.js
+++ server/routes/artsRoute.js
@@ -5,8 +5,6 @@ const artController=require("../controllers/artController")
 const isManager=require("../middleware/isManager")
 const verifyJWT=require("../middleware/verifyJWT")
 const createNewOrder=require("../controllers/orderController")
-
-
 router.get("/",artController.getAllArts)
 router.get("/:id",artController.getArtByID)
 router.post("/",artController.createNewArt)
